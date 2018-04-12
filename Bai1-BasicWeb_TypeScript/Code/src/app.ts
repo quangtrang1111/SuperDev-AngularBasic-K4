ff3474596f2adb53353eb723e85139ab448de81e
Quang Trang <huynhquangtrang1994@gmail.com>
2018-04-12 21:52:46 +0700

    add

--- Bai1-BasicWeb_TypeScript/Code/src/app.ts
+++ Bai1-BasicWeb_TypeScript/Code/src/app.ts
@@ -1,13 +1,10 @@
+import { Person } from "./peson";
+
 class Student {
     fullName: string;
     constructor(public firstName, public middleInitial, public lastName) {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
-}
-
-interface Person {
-    firstName: string;
-    lastName: string;
 }
 
 function greeter(person : Person) {
